bce9b03ec659dd3d1af3070748b5a70ece58d22d
ezrokaelthas <80545826+ezrokaelthas@users.noreply.github.com>
2022-12-02 02:14:38 +0800

    Add files via upload

---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typed', {
+    strings: ['Developer', 'Designer', 'Freelancer', 'Content Creator'],
+    smartBackspace: true,
+    loop: true,
+    typeSpeed: 90,
+    backSpeed: 40
+});
